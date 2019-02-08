f37571463719ba87950418c6f4a7025e5c4ad23b
sanjithpk <sanjithpk06@gmail.com>
2019-02-08 16:09:53 +0530

    adding particles.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/navigation/Navigation';
 import Logo from './components/logo/Logo'
 import Rank from './components/rank/Rank'
 import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'
 import './App.css';
 
+const ParticlesOptions = {
+  particles: {
+    number: {
+      value: 160,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={ParticlesOptions}
+        />
         <Navigation/>
         <Logo />
         <Rank/>
