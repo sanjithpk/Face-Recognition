64d53f4cbe0bc8f64370fa0d98650c8bb088c916
sanjithpk <sanjithpk06@gmail.com>
2019-06-14 12:49:23 +0530

    Completed the front end

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
-import Navigation from './components/navigation/Navigation';
-import Logo from './components/logo/Logo'
-import Rank from './components/rank/Rank'
-import ImageLinkForm from './components/imageLinkForm/ImageLinkForm'
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import Rank from './Components/Rank/Rank';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import './App.css';
 
-const ParticlesOptions = {
+const app = new Clarifai.App({
+  apiKey: '6123f19628954f1ca72a66871ca6b04e'
+ });
+
+const particlesOptions = {
   particles: {
     number: {
-      value: 150,
+      value: 180,
       density: {
         enable: true,
         value_area: 800
@@ -19,21 +25,57 @@ const ParticlesOptions = {
 }
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Particles className='particles'
-          params={ParticlesOptions}
-        />
-        <Navigation/>
-        <Logo />
-        <Rank/>
-        <ImageLinkForm/>
-        {
-        // <faceRecognition />
-        }
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace= data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height),
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box})
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+render() {
+  return (
+     <div className="App">
+       <Particles className='particles' params={particlesOptions}/>
+       <Navigation />
+       <Logo />
+       <Rank />
+       <ImageLinkForm onInputChange = {this.onInputChange} onButtonSubmit = {this.onButtonSubmit} />
+       <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+     </div>
+   );
   }
 }
 
--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Tilt from 'react-tilt';
-import brain from './brain.png'
 import './Logo.css';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt" options={{ max : 25 }} style={{ height: 100, width: 100 }} >
-                <div className="Tilt-inner">
-                    <img style={{padding: '5px' ,height: '90px', width: '90px'}} alt='logo' src={brain }></img>
-                </div>
-            </Tilt>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+        <div className="Tilt-inner pa3">
+          <img style={{paddingTop: '5px'}} alt='logo' src='https://raw.githubusercontent.com/aneagoie/face-recognition-brain/master/src/components/Logo/brain.png' />
         </div>
+      </Tilt>
+    </div>
     );
 }
 
