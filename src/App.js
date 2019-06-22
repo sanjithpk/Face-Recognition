7548e1e8e2cfa6c540343a82c24df97882a8822e
sanjithpk <sanjithpk06@gmail.com>
2019-06-22 19:57:12 +0530

    connecting to backend

--- src/App.js
+++ src/App.js
@@ -38,8 +38,6 @@ class App extends Component {
     }
   }
 
-
-
   calculateFaceLocation = (data) => {
     const clarifaiFace= data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputImage');
@@ -51,7 +49,7 @@ class App extends Component {
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height),
     }
-  } 
+  }
 
   displayFaceBox = (box) => {
     this.setState({box: box})
